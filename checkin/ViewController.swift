f628a2314c87aaaae0a0e8bca8bf25d9df6f2fe5
LiJohnson <598420668@163.com>
2016-11-02 17:41:26 +0800

    Initial Commit

---
+++ checkin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  checkin
+//
+//  Created by lcs on 02/11/2016.
+//  Copyright © 2016 lcs.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
