7a56d49951091aceeb0a23094a825c4b13177e79
LiJohnson <598420668@163.com>
2016-11-03 23:37:00 +0800

    add input

--- checkin/ViewController.swift
+++ checkin/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var dateText: UITextField!
+   
+    @IBAction func datePicker(_ sender: UITextField) {
+        let datePickerView:UIDatePicker = UIDatePicker()
+        
+        datePickerView.datePickerMode = UIDatePickerMode.date
+        
+        sender.inputView = datePickerView
+        
+        datePickerView.addTarget(self, action: #selector(ViewController.datePickerValueChanged), for: UIControlEvents.valueChanged)
+
+    }
+    
+    func datePickerValueChanged(sender:UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale =  Locale(identifier: "zh_CN")
+        dateFormatter.dateFormat = "yyyy-MM-dd EEEE"
+        dateText.text = dateFormatter.string(from: sender.date)
+        
+    }
+    
+    @IBAction func checkin(_ sender: UIButton) {
+        print(userName.text!)
+        print(password.text!)
+        print(dateText.text!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale =  Locale(identifier: "zh_CN")
+        dateFormatter.dateFormat = "yyyy-MM-dd EEEE"
+        dateText.text = dateFormatter.string(from: Date())
     }
 
     override func didReceiveMemoryWarning() {
